e6aba11e82682c1b7494476e7a547b54b55b267c
IvayloTsankov <zub40000@gmail.com>
2015-07-05 15:03:54 +0000

    * Rename class

---
+++ src/motion.py
@@ -0,0 +1,15 @@
+class Motion:
+    def __init__(self):
+        pass
+
+    def forward(self, power, time):
+        pass
+
+    def backward(self, power, time):
+        pass
+
+    def turn_left(self, time):
+        pass
+
+    def turn_right(self, time):
+        pass
