63527d7f13fa64735e9a757f96c0035204565303
IvayloTsankov <zub40000@gmail.com>
2015-07-09 21:02:56 +0000

    * Rename class

---
+++ src/bluetooth.py
@@ -0,0 +1,25 @@
+class Bluetooth:
+    def __init__(self, name, on_open, on_message, on_close, on_error):
+        self.name = name
+        self.buff = ""
+        self.on_open = on_open
+        self.on_message = on_message
+        self.on_close = on_close
+        self.on_error = on_error
+    
+    def discover(self, timeout):
+        pass
+
+    def connect(self, remote_device):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def send(self, message):
+        # send the message as byte string
+        pass
+
+    def recieve(self, num_of_bytes):
+        # return message
+        pass
