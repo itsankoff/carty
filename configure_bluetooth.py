4465595924f960d4ed3dcdc104668a843c38b935
root <root@raspberrypi.(none)>
2015-07-10 08:36:22 +0000

    * Add tool to configure bluetooth module

---
+++ configure_bluetooth.py
@@ -0,0 +1,45 @@
+import serial
+
+
+# supported commands for HC-06 bluetooth serial module
+commands = {"AT": "check connectivity",
+            "AT+NAME": "set name of device (no space between cmd and name",
+            "AT+BAUD": "set baudrate of device (e.g 9600) (no space again)",
+            "AT+PIN": "set pin code of device (no space again",
+            "AT+VERSION": "get version"}
+
+
+def print_help():
+    commands["exit"] = "exit"
+    commands["help"] = "show this section"
+
+    for command, description in commands.items():
+        print("{} - {}".format(command, description))
+
+
+def main():
+    s = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
+    print("connected to: " + s.portstr)
+    print("insert commands:")
+
+    while True:
+        command = input(">>")
+        if command == "exit":
+            break
+
+        if command == "help":
+            print_help()
+            continue
+
+        # commands to bluetooth
+        s.write(command.encode("ascii"))
+
+        buff = s.read(50).decode(encoding='UTF-8')
+        if buff:
+            print(buff)
+
+    s.close()
+
+
+if __name__ == "__main__":
+    main()
