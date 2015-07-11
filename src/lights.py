d92d12c772120449a5042b3c3d7a78fb4902d9e0
IvayloTsankov <zub40000@gmail.com>
2015-07-11 10:34:28 +0000

    * Add light module again (broken repo)

--- src/lights.py
+++ src/lights.py
@@ -5,17 +5,19 @@ import RPi.GPIO as gpio
 class Lights:
     def __init__(self):
         config = configparser.ConfigParser()
-        config.read("carty.ini")
+        config.read("../carty.ini")
         lights_config = config["LIGHTS"]
 
-        self.FRONT_LIGHTS = lights_config["FRONT_LIGHTS"]
-        self.BACK_LIGHTS = lights_config["BACK_LIGHTS"]
+        self.FRONT_LIGHTS = int(lights_config["FRONT_LIGHTS"])
+        self.BACK_LIGHTS = int(lights_config["BACK_LIGHTS"])
 
-        gpio.setup(self.FRONT_LIGHTS, OUT)
-        gpio.setup(self.BACK_LIGHTS, OUT)
+        gpio.setmode(gpio.BOARD)
+        gpio.setup(self.FRONT_LIGHTS, gpio.OUT)
+        gpio.setup(self.BACK_LIGHTS, gpio.OUT)
 
-        self.disable_front_lights()
-        self.disable_back_light()
+        # In Europe front lights are obligatory
+        self.enable_front_lights()
+        self.disable_back_lights()
 
     def enable_front_lights(self):
         gpio.output(self.FRONT_LIGHTS, gpio.HIGH)
